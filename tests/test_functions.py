9392f10911a41b2d490713d522ef0ee32dafaf55
Danis Akhmadiev <iamdanisakhmadiev>
2024-03-05 18:02:57 +0400

    fixed total unique products and tests, added .gitignore

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,12 +1,12 @@
 import pytest
-from main import Category, Product
+from main.functions import Category, Product
 @pytest.fixture
 def pizza_category():
     return Category('Pizzas', 'pizza products', "Margarita")
 @pytest.fixture
 def pizza_margarita():
     return Product('Margarita', 'classical pizza with tomatos', 450, 30)
-
+@pytest.fixture()
 def test_init(pizza_category):
     assert pizza_category.name == 'Pizzas'
     assert pizza_category.description == 'pizza products'
@@ -17,6 +17,8 @@ def test_init(pizza_margarita):
     assert pizza_margarita.description == 'classical pizza with tomatos'
     assert pizza_margarita.price == 450
     assert pizza_margarita.available == 30
+    Category.update_unique_products()
     assert Category.total_unique_products == 1
+
 
 
